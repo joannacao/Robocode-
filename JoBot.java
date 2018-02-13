4e6729194e050c03124f81cf3b6e706ad5b2ce61
joannacao <35121742+joannacao@users.noreply.github.com>
2018-02-13 02:10:36 -0800

    Comments

--- JoBot.java
+++ JoBot.java
@@ -1,10 +1,9 @@
 package JC;
 import robocode.*;
-//import robocode.util.*;  
-//import java.util.*; 
 import java.awt.Color;
 
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+// Where I found dodgebot code: https://www.ibm.com/developerworks/library/j-dodge/index.html
 
 /**
  * JoBot - a robot by (your name here)
@@ -16,17 +15,10 @@ public class JoBot extends AdvancedRobot
 	 */
 	double prevEnergy = 100.00;  
 	public void run() {
-	
-		//WHILE GUN IS COOLING DOWN, HAVE BOT MOVE AS MUCH AS POSSIBLE TO AVOID GETTING HIT
-		// Initialization of the robot should be put here
-
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-
 		setColors(Color.red,Color.blue,Color.green); // body,gun,radar
 
 		// Robot main loop
-		setTurnGunRight(9999); 
+		setTurnGunRight(9999); //constantly rotates gun to scan for robots
 	}
 
 	/**
@@ -35,38 +27,38 @@ public class JoBot extends AdvancedRobot
 	//create shooting strategies: head on, circular, pattern
 	//MAYBE: guess factor targeting because it's complex
 	public void onScannedRobot(ScannedRobotEvent e) {
+		//turns right so it's easier to move (can just use setAhead or setBack)
 		setTurnRight(e.getBearing() + 90); 
 		
+		//if there is a slight change in the other bot's energy (meaning a bullet was just fired) 
 		double changeEnergy = prevEnergy - e.getEnergy(); 
 		if (changeEnergy >= 0 && changeEnergy <= 3){
-			setBack(60); 
+			setBack(60); //the bot moves to dodge
 		}
 		
-		setTurnGunLeft(9999);
+		setTurnGunLeft(9999); //spins radar once again and then fires
 		if (e.getDistance() < 100){
-			fire(3); 
+			fire(3); //fires more if the bot is closer
 		} else {
 			fire(1);
 		}
 		
-		prevEnergy = e.getEnergy(); 
+		prevEnergy = e.getEnergy(); //resets prevEnergy to use when this event is called again
 	}
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
 		back(10);
-		fire(1); 
+		fire(1); //back up and fire again
 	}
 	
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
-		setTurnLeft(90); 
+		back(20); 
+		setTurnLeft(90); //if moving back doesn't move it away from the wall, turning left/right should
 	}	
 }
